f024d6d25167324bd281770c253c59c032d1456e
unknown <nsyboss06@gmail.com>
2026-03-20 14:17:18 +0100

    Add properly named Python file

---
+++ first_solo_project.py
@@ -0,0 +1,13 @@
+print('----MY PERSONAL BODY MASS INDEX(BMI) CALCULATOR----')
+weight=float(input('Please Enter Your Weight In Kilograms: '))
+height=float(input('Please Enter Your Height In Meters: '))
+bmi=weight/(height**2)
+print(f'Your BMI is: {bmi}')
+if bmi <= 18.5:
+    print('You Are Underweight')
+elif bmi >18.5 and bmi <= 24.9:
+    print('You Have A Normal Weight')
+elif bmi > 24.9 and bmi <= 29.9:
+    print('You Are Overweight')
+elif bmi > 29.9 and bmi <= 34.9:
+    print('You Are Obese')
