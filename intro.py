2d67b9f01bac09d22622ff6d29a06b23f1a218c5
unknown <nsyboss06@gmail.com>
2026-03-20 07:36:26 +0100

    Clean commit with only code files

--- intro.py
+++ intro.py
@@ -1,4 +1,36 @@
+
+# these are comments
+# this is to explain what integers are and what are operations they use
+print(7+9)
+print(49-11)
+print(64*128)
+print(512/1024)
+print(7.9*7.9)
+print(8**4)
+print(32//128)
+print(32%128)
+# this is to show the type of data type being used here
+print(type(7+9))
+print(type(49-11))
+print(type(64*128))
+print(type(512/1024))
+print(type(7.9*7.9))
+print(bin(11))
+print(int('0b1011', 2))
+print(round(49/8))
+print(abs(-1738))
+# these are augmented assignment operators
+u = 6
+u *= 10
+print(u)
+# these are talking about variables
+id = 570
+age_limits = (id/8)
+print(age_limits)
+print(id)
 v = input('Please Enter Your Name: ')
-print('hello world')
+# this is my first python lesson
+print(type(v))
+print("hello world")
 print('My Name Is ' + v)
 print(f'Hello {v} And Welcome To The World Of Python Programming')
